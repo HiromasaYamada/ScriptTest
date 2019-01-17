b2ac96da1034d28e06b26974384ba8dfcc6619c5
hiro_yamada <ashitaka0789@gmail.com>
2019-01-18 05:35:21 +0900

    ScriptTest C# スクリプト基礎
    5つの要素を順番→逆順に表示

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        int[] points = { 0, 1, 2, 3, 4 };
+
+        for (int i = 0; i < points.Length; i++) {
+            Debug.Log(points[i]);
+            }
+        for (int i = 4; i < points.Length; i--) {
+            Debug.Log(points[i]);
+            }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
